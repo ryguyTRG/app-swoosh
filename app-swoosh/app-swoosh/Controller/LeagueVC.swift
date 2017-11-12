6ce8b3b236f34284135d8477f80f2257e3d969a2
Ryan Gjoraas <ryangjoraas@Ryans-MacBook-Pro.local>
2017-11-11 22:25:26 -0600

    added segue programmatically

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,40 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Ryan Gjoraas on 11/11/17.
+//  Copyright © 2017 Gorgeous Running. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
