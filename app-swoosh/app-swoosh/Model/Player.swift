55e99c99664b4106fdc30322651ea1f82b794831
Ryan Gjoraas <ryangjoraas@Ryans-MacBook-Pro.local>
2017-11-11 23:12:27 -0600

    added data model class

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ryan Gjoraas on 11/11/17.
+//  Copyright © 2017 Gorgeous Running. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
